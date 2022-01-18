aba33728165803a4d793c4187b1b997324dc5ea2
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2022-01-18 22:43:38 +0330

    added: get_or_none, debug_p.

--- pynight/common_debugging.py
+++ pynight/common_debugging.py
@@ -1,5 +1,8 @@
 import sys
 import traceback
+import os
+
+debug_p = 'DEBUGME' in os.environ
 
 def traceback_print(file=None):
     print(traceback.format_exc(), file or sys.stderr)
--- pynight/common_iterable.py
+++ pynight/common_iterable.py
@@ -1,4 +1,12 @@
+##
 def iterable_chunk(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+##
+def get_or_none(lst, n):
+    if n in lst:
+        return lst[n]
+    else:
+        return None
+##
