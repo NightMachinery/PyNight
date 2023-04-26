5628c0d3e0c1ccd3fb8607f7b0f65d20f128514a
NightMachinery <feraidoonmehri@gmail.com>
2023-04-26 14:49:00 +0330

    clipboard_copy_jupyter: improved

--- pynight/common_jupyter.py
+++ pynight/common_jupyter.py
@@ -2,9 +2,9 @@ import uuid
 from IPython.display import display, display_javascript, HTML
 from .common_json import JSONEncoderWithFallback
 
-def clipboard_copy_jupyter(text):
-    json_encoder = JSONEncoderWithFallback(fallback_function=str, indent=2)
-    escaped_text = json_encoder.encode(text)
+def clipboard_copy_jupyter(obj, indent=4):
+    json_encoder = JSONEncoderWithFallback(fallback_function=str, indent=indent)
+    obj_json = json_encoder.encode(obj)
 
     button_id = f"copy-btn-{uuid.uuid4()}"
     html_code = f"""
@@ -14,21 +14,33 @@ def clipboard_copy_jupyter(text):
     """
 
     js_code = f"""
+    function updateButton(button_id, message, color) {{
+        const button = document.getElementById(button_id);
+        button.style.backgroundColor = color || "#f44336";
+        button.innerText = message;
+    }}
+
     function copyToClipboard() {{
-        const textToCopy = {escaped_text};
+        updateButton("{button_id}", "Copying ...", "rebeccapurple");
+
+        var textToCopy = {obj_json};
+        if (typeof textToCopy !== 'string') {{
+            try {{
+                textToCopy = JSON.stringify(textToCopy, null, {indent});
+            }} catch (err) {{
+                console.error("Error stringifying some_var:", err);
+                updateButton("{button_id}", "Error: " + err.message);
+                return;
+            }}
+        }}
 
         navigator.clipboard.writeText(textToCopy)
             .then(() => {{
-                const button = document.getElementById("{button_id}");
-                button.style.backgroundColor = "#008CBA";
-                button.innerText = "Copied!";
+                updateButton("{button_id}", "Copied!", "#008CBA");
             }})
             .catch(err => {{
                 console.error('Unable to copy text: ', err);
-
-                const button = document.getElementById("{button_id}");
-                button.style.backgroundColor = "#f44336";
-                button.innerText = "Error: " + err.message;
+                updateButton("{button_id}", "Error: " + err.message);
             }});
     }}
 
@@ -37,3 +49,5 @@ def clipboard_copy_jupyter(text):
 
     display(HTML(html_code))
     display_javascript(js_code, raw=True)
+
+    return obj_json
