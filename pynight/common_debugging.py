baa758b77da8c6c8b62c5a3be01a824ff3e8d70b
NightMachinery <feraidoonmehri@gmail.com>
2023-05-07 15:58:40 +0330

    added stacktrace_caller_line

--- pynight/common_debugging.py
+++ pynight/common_debugging.py
@@ -8,8 +8,9 @@ debug_p = os.environ.get("DEBUGME", None)
 def traceback_print(file=None):
     print(traceback.format_exc(), file or sys.stderr)
 
+
 ##
-def stacktrace_get(back=0, skip_last=2, mode='line'):
+def stacktrace_get(back=0, skip_last=2, mode="line"):
     """
     Returns a formatted stacktrace string, showing a specified number of calls
     before the current function call. This is useful for debugging purposes.
@@ -25,7 +26,7 @@ def stacktrace_get(back=0, skip_last=2, mode='line'):
         str: Formatted stacktrace string.
     """
 
-    back += 1 #: to account for this function
+    back += 1  #: to account for this function
 
     stack = traceback.extract_stack()
 
@@ -34,10 +35,16 @@ def stacktrace_get(back=0, skip_last=2, mode='line'):
 
     formatted_stacktrace = f""
     for frame in stack[start_index:end_index]:
-        if mode == 'full':
+        if mode == "full":
             formatted_stacktrace += f'File "{frame.filename}", line {frame.lineno}, in {frame.name}\n    {frame.line}\n'
-        elif mode == 'line':
-            formatted_stacktrace += f'{frame.line}\n'
+        elif mode == "line":
+            formatted_stacktrace += f"{frame.line}\n"
 
     return formatted_stacktrace
+
+
+def stacktrace_caller_line():
+    return stacktrace_get(back=0, skip_last=3, mode="line")
+
+
 ##
