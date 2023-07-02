81518f7ca3b2e58c35ecf0daaebbb4476e8dba6a
NightMachinery <feraidoonmehri@gmail.com>
2023-07-02 21:26:30 +0330

    fn_name: supports partial

--- pynight/common_functional.py
+++ pynight/common_functional.py
@@ -7,6 +7,11 @@ def fn_name(fn, ):
     # return rget(str(fn), r'^<function (\S+)')
     ##
     #: [[https://stackoverflow.com/questions/58108488/what-is-qualname-in-python][What is __qualname__ in python? - Stack Overflow]]
+    if not hasattr(fn, '__qualname__'):
+        if hasattr(fn, 'func'):
+            #: a functools.partial wrapped function
+            fn = fn.func
+
     return f"{fn.__module__}.{fn.__qualname__}"
 
     # return fn.__name__
