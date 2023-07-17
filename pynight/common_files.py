5019bc048b3fff4ba5a2a81f20b7f65c0ee021a5
Feraidoon <feraidoonmehri@gmail.com>
2023-07-17 21:06:49 +0330

    added rm

--- pynight/common_files.py
+++ pynight/common_files.py
@@ -1,7 +1,11 @@
 import io
 import pathlib
+from pathlib import Path
+import shutil
+from typing import Union
+from pynight.common_dict import simple_obj
 
-Path = pathlib.Path
+
 ##
 def cat(file_path):
     with io.open(file_path, "r", encoding="utf8") as file_:
@@ -17,14 +21,44 @@ def mkdir(path):
 
 dir_ensure = mkdir
 ensure_dir = mkdir
+
+
 ##
 def sanitize_filename(some_str):
     for x in (
-            '/',
-            '\\',
-            '~',
-            ):
-        some_str = some_str.replace(x, '_')
+        "/",
+        "\\",
+        "~",
+    ):
+        some_str = some_str.replace(x, "_")
 
     return some_str
+
+
+##
+def rm(path):
+    path = Path(path)  #: make sure path is a Path object
+
+    if not path.exists():
+        return simple_obj(
+            retcode=1,
+            msg=f"The path {path} does not exist.",
+        )
+
+    if path.is_file() or path.is_symlink():
+        path.unlink()
+    elif path.is_dir():
+        shutil.rmtree(path)  #: remove dir and all contains
+    else:
+        return simple_obj(
+            retcode=2,
+            msg=f"Unknown type: {path}",
+        )
+
+    return simple_obj(
+        retcode=0,
+        msg=f"Removed: {path}",
+    )
+
+
 ##
