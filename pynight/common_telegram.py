38763abac7f97c8b33cb0a2dfb30862f44beaed6
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-07-27 18:32:04 +0330

    telegram: refactored send_file into send, added lock_key

--- pynight/common_telegram.py
+++ pynight/common_telegram.py
@@ -1,10 +1,17 @@
+from typing import Iterable
 from pathlib import Path
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import tempfile
 import subprocess
 import os
 from brish import zn
+from pathlib import Path
+import tempfile
+from collections import defaultdict
+import concurrent.futures
+from pynight.common_icecream import ic
 
 
 ##
@@ -14,26 +21,50 @@ def log_tlg(message, chat_id=None):
 
 
 ##
-def send_file(file, chat_id, msg="", wait_p=False, savefig_opts=None, lock_path=None):
+#: Create a ThreadPoolExecutor with a single worker thread for each lock_key
+lock_key_executors = defaultdict(
+    lambda: concurrent.futures.ThreadPoolExecutor(max_workers=1)
+)
+
+
+def send(
+    chat_id,
+    files=None,
+    msg="",
+    wait_p=False,
+    savefig_opts=None,
+    lock_path=None,
+    lock_key=None,
+):
     chat_id = chat_id or os.environ.get("tlogs", None)
     savefig_opts = savefig_opts or dict()
 
-    # Handle case if file is a matplotlib plot
-    if isinstance(file, Figure):
-        with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp:
-            file.savefig(temp.name, format="png", **kwargs)
-            file_path = temp.name
-    # If file is a string or a Path object
-    elif isinstance(file, (str, Path)):
-        file_path = str(file)
-    else:
-        raise ValueError(f"Unsupported type: {type(file)}")
-
     cmd = [
         "tsend.py",
-        "--file",
-        file_path,
     ]
+
+    if files is None:
+        files = []
+    if isinstance(files, str) or not isinstance(files, Iterable):
+        files = [files]
+
+    for file in files:
+        # Handle case if file is a matplotlib plot
+        if isinstance(file, Figure):
+            with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp:
+                file.savefig(temp.name, format="png", **savefig_opts)
+                file_path = temp.name
+        # If file is a string or a Path object
+        elif isinstance(file, (str, Path)):
+            file_path = str(file)
+        else:
+            raise ValueError(f"Unsupported type: {type(file)}")
+
+        cmd += [
+            "--file",
+            file_path,
+        ]
+
     if lock_path:
         cmd += [
             "--lock-path",
@@ -46,11 +77,25 @@ def send_file(file, chat_id, msg="", wait_p=False, savefig_opts=None, lock_path=
         msg,
     ]
 
-    # If we need to wait for the command to finish
-    if wait_p:
-        subprocess.check_call(cmd)
+    if lock_key is not None:
+        # Use the executor associated with this lock_key to run the command
+        executor = lock_key_executors[lock_key]
+
+        future = executor.submit(_run_cmd, cmd)
+        if wait_p:
+            return future.result()
+        else:
+            # ic(future, cmd)
+            return future
     else:
-        subprocess.Popen(cmd)
+        if wait_p:
+            subprocess.check_call(cmd)
+        else:
+            subprocess.Popen(cmd)
+
+
+def _run_cmd(cmd):
+    subprocess.check_call(cmd)
 
 
 ##
