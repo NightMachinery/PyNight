127174e254106e3adbb0368b40c7b085b60f9638
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-07-27 17:14:12 +0330

    icecream: fixed bug, added ic_colorize2

--- pynight/common_icecream.py
+++ pynight/common_icecream.py
@@ -2,7 +2,14 @@ from pynight.common_condition import jupyter_p
 from icecream import ic, colorize as ic_colorize
 
 
-if jupyter_p:
+def ic_colorize2(input):
+    input_str = str(input)
+
+    return ic_colorize(input_str)
+    # return input_str
+
+
+if jupyter_p():
     ic.configureOutput(outputFunction=lambda s: print(s))
 else:
-    ic.configureOutput(outputFunction=lambda s: print(ic_colorize(s)))
+    ic.configureOutput(outputFunction=lambda s: print(ic_colorize2(s)))
