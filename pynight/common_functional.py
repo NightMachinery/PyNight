52664e6d7c92fb499f7a1df8973dfed63f7535fc
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-07-31 19:12:46 +0330

    fn_name: added module_p

--- pynight/common_functional.py
+++ pynight/common_functional.py
@@ -2,18 +2,25 @@ from .common_regex import rget
 
 
 ##
-def fn_name(fn, ):
+def fn_name(fn, module_p=True):
     ##
     # return rget(str(fn), r'^<function (\S+)')
     ##
     #: [[https://stackoverflow.com/questions/58108488/what-is-qualname-in-python][What is __qualname__ in python? - Stack Overflow]]
-    if not hasattr(fn, '__qualname__'):
-        if hasattr(fn, 'func'):
+    if not hasattr(fn, "__qualname__"):
+        if hasattr(fn, "func"):
             #: a functools.partial wrapped function
             fn = fn.func
 
-    return f"{fn.__module__}.{fn.__qualname__}"
+    res = ""
+    if module_p:
+        res += f"{fn.__module__}."
+
+    res += f"{fn.__qualname__}"
+    return res
 
     # return fn.__name__
     ##
+
+
 ##
