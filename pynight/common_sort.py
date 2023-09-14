1ed4314bd7a7db4e0c22810c691120382ab03901
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-09-14 14:18:55 +0330

    version_sort_key: added float_p

--- pynight/common_sort.py
+++ pynight/common_sort.py
@@ -1,18 +1,35 @@
 import re
 import os
 
+
 ##
 def identity(x):
     return x
 
 
-def version_sort_key(s, pre_key_fn=identity):
+def version_sort_key(s, pre_key_fn=identity, float_p=False):
     """Sort strings containing numbers in a way that the numbers are considered as a whole,
     not individual characters (e.g. 10 comes after 2)"""
-    return [
-        int(text) if text.isdigit() else text.lower()
-        for text in re.split("(\d+)", pre_key_fn(s))
-    ]
+
+    int_pattern = re.compile("(\d+)")
+    float_pattern = re.compile("(\d+\.\d+|\d+)")
+
+    split_pattern = float_pattern if float_p else int_pattern
+
+    result = []
+    for text in split_pattern.split(pre_key_fn(s)):
+        try:
+            if float_p:
+                num = float(text)
+            else:
+                num = int(text)
+
+            result.append(num)
+        except ValueError:
+            # If it's not a number, treat it as a string
+            result.append(text)
+
+    return result
 
 
 ##
