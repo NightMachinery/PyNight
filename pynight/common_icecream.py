87bcd1c17204745993f96cc75bf6da91b67f9e85
NightMachinery <feraidoonmehri@gmail.com>
2023-09-30 00:27:02 +0330

    added ic_output

--- pynight/common_icecream.py
+++ pynight/common_icecream.py
@@ -2,6 +2,10 @@ from pynight.common_condition import jupyter_p
 from pynight.common_debugging import traceback_print
 from icecream import ic
 from icecream import colorize as ic_colorize
+import sys
+
+
+ic_output = sys.stdout
 
 
 def ic_colorize2(input):
@@ -15,7 +19,11 @@ if jupyter_p():
 
     def _ic_print(s):
         try:
-            print(s)
+            print(
+                s,
+                file=ic_output,
+                flush=True,
+            )
         except:
             traceback_print()
 
@@ -23,7 +31,11 @@ else:
 
     def _ic_print(s):
         try:
-            print(ic_colorize2(s))
+            print(
+                ic_colorize2(s),
+                file=ic_output,
+                flush=True,
+            )
         except:
             traceback_print()
 
