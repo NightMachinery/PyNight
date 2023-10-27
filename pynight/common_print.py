31895bc4aec52071ae0a2ad2a9164299a6ed2040
NightMachinery <feraidoonmehri@gmail.com>
2023-10-27 14:58:45 +0330

    new common_print.py

---
+++ pynight/common_print.py
@@ -0,0 +1,55 @@
+from pprint import pformat
+from typing import Any
+
+from pygments import highlight
+from pygments.formatters import Terminal256Formatter
+from pygments.lexers import PythonLexer
+
+from pynight.common_icecream import ic_colorize2
+
+
+##
+def pprint_color(
+    obj: Any,
+    color="ic",
+    end=None,
+    pformat_opts=None,
+    **kwargs,
+) -> None:
+    """Pretty-print in color."""
+    if pformat_opts is None:
+        pformat_opts = dict()
+
+    res = pformat(
+        obj,
+        **pformat_opts,
+    )
+
+    if not color:
+        pass
+    elif str(color) == "ic":
+        res = ic_colorize2(res)
+    elif str(color) == "256":
+        res = highlight(
+            res,
+            PythonLexer(),
+            Terminal256Formatter(),
+        )
+
+        if end is None:
+            end = ""
+            #: =highlight= adds a newline.
+    else:
+        raise ValueError(f"Unknown color: {color}")
+
+    if end is None:
+        end = "\n"
+
+    print(
+        res,
+        end=end,
+        **kwargs,
+    )
+
+
+##
