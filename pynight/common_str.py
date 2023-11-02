86ec5ca5bfd2e896297ee1fad3aeea2e890c46f7
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-11-02 21:26:40 +0330

    added print_to_file

--- pynight/common_str.py
+++ pynight/common_str.py
@@ -1,3 +1,18 @@
+import sys
+
+
+##
+def print_to_file(*args, file=sys.stdout, **kwargs):
+    if isinstance(file, str):
+        file_obj = open(file, 'a')
+    else:
+        file_obj = file
+
+    try:
+        print(*args, file=file_obj, **kwargs)
+    finally:
+        if isinstance(file, str):
+            file_obj.close()
 ##
 def try_float(value):
     try:
