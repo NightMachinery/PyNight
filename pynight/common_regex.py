d5a789db4f227c262079e39389c2bd8b1a49a288
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-11-02 21:26:30 +0330

    added float_pattern

--- pynight/common_regex.py
+++ pynight/common_regex.py
@@ -2,6 +2,11 @@ import re
 
 
 ##
+float_pattern = r"\d+(?:\.\d*)?"
+float_pattern_compiled = re.compile(float_pattern)
+
+
+##
 def rget(input, pattern):
     m = re.compile(f".*?{pattern}.*").match(input)
     if m:
