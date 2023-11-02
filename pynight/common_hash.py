2886e4118bea9804eaf5b02aed5dbdd05759afc5
Feraidoon Mehri <feraidoonmehri@gmail.com>
2023-11-02 21:24:16 +0330

    added hashable_p

--- pynight/common_hash.py
+++ pynight/common_hash.py
@@ -1,12 +1,22 @@
 import hashlib
 from urllib.parse import urlparse
 import os
+from collections.abc import Hashable
 
 
+##
+def hashable_p(obj):
+    return isinstance(obj, Hashable)
+
+
+is_hashable = hashable_p
+
+
+##
 def hash_url(url, length=10):
     basename = os.path.basename(urlparse(url).path)
 
-    hash_object = hashlib.sha1(url.encode('utf-8'))
+    hash_object = hashlib.sha1(url.encode("utf-8"))
 
     short_hash = hash_object.hexdigest()[:length]
 
@@ -14,3 +24,6 @@ def hash_url(url, length=10):
         return f"{basename}_{short_hash}"
     else:
         return short_hash
+
+
+##
