46944847c7e090f5872baeddeb413a107e5fbe45
NightMachinery <feraidoonmehri@gmail.com>
2024-01-03 00:52:16 +0330

    added clipboard_copy_multi_sync

--- pynight/common_clipboard.py
+++ pynight/common_clipboard.py
@@ -1,5 +1,24 @@
 import pyperclip
+import threading
+import time
 
 
 def clipboard_copy(obj):
     return pyperclip.copy(obj)
+
+
+def clipboard_copy_multi_sync(
+    texts,
+    sleep=0.5,
+):
+    for text in texts:
+        if text:  #: Check if the text is not empty or None
+            clipboard_copy(text)
+            time.sleep(sleep)
+            #: to allow polling-based clipboard managers to capture the text
+
+
+def clipboard_copy_multi(*args):
+    copy_thread = threading.Thread(target=clipboard_copy_multi_sync, args=(args,))
+
+    copy_thread.start()
