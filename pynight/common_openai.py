ba07393625d8336cf1c59cdeea1131835b7e91e6
NightMachinery <feraidoonmehri@gmail.com>
2024-01-03 00:53:33 +0330

    added print_chat_streaming

--- pynight/common_openai.py
+++ pynight/common_openai.py
@@ -41,49 +41,130 @@ import time
 import sys
 
 
+def print_chat_streaming(
+    output,
+    *,
+    debug_p=False,
+    # debug_p=True,
+    output_mode=None,
+    copy_mode="chat2",
+    # copy_mode="default",
+    end="\n-------",
+):
+    """
+    Process and print out chat completions from a model when the stream is set to True.
+
+    Args:
+        output (iterable): The output from the model with stream=True.
+    """
+    text = ""
+    r = None
+    for i, r in enumerate(output):
+        if not isinstance(r, dict):
+            #: OpenAI v1: Response objects are now pydantic models instead of dicts.
+            ##
+            r = dict(r)
+
+        text_current = None
+        choice = r["choices"][0]
+        if "delta" in choice:
+            delta = choice["delta"]
+            if i >= 1:
+                #: No need to start all responses with 'assistant:'.
+                ##
+                if "role" in delta:
+                    if i >= 1:
+                        print("\n", end="")
+
+                    print(f"{delta['role']}: ", end="")
+
+            if "content" in delta:
+                text_current = f"{delta['content']}"
+        elif "text" in choice:
+            text_current = f"{choice['text']}"
+
+        if text_current:
+            text += text_current
+            print(f"{text_current}", end="")
+
+    print(end, end="")
+
+    text = text.rstrip()
+
+    if debug_p == True:
+        ic(r)
+
+    chat_result = None
+    if copy_mode:
+        chat_result = chatml_response_text_process(
+            text,
+            copy_mode=copy_mode,
+        )
+
+    if output_mode == "chat":
+        return chat_result
+    elif output_mode == "text":
+        return text
+    elif not output_mode:
+        return None
+    else:
+        raise ValueError(f"Unsupported output_mode: {output_mode}")
+
+
+def chatml_response_process(
+    response,
+    end="\n-------",
+    **kwargs,
+):
+    for choice in response["choices"]:
+        text = choice["message"]["content"]
+
+        chatml_response_text_process(
+            text,
+            **kwargs,
+        )
+        print(text, end="")
+
+        print(end, end="")
+
+
 def chatml_response_text_process(
     text,
-    copy_mode="default",
+    copy_mode="chat2",
+    # copy_mode="default",
 ):
     #: 'rawchat' is currently useless, just use 'text'.
     ##
     text_m = None
     if copy_mode in ("chat", "chat2"):
-        text_m = f"""        {{"role": "assistant", "content": '''{text}'''}},"""
+        text_m = f'''        {{"role": "assistant", "content": r"""{text}"""}},'''
     elif copy_mode in ("rawchat"):
         text_m = f"""{text}"""
 
     if copy_mode == "chat2":
-        text_m += f"""
-        {{"role": "user", "content": ''' '''}},"""
+        text_m += f'''
+        {{"role": "user", "content": r"""\n        \n        """}},'''
 
-    if copy_mode == "default":
-        pyperclip.copy(text)
+    if copy_mode in (
+        "default",
+        "chat2",
+    ):
+        clipboard_copy_multi(text, text_m)
 
-        time.sleep(0.1)
-        #: to allow polling-based clipboard managers to capture the text
-
-        pyperclip.copy(text_m)
-
-    elif copy_mode in ("chat", "chat2", "rawchat"):
-        pyperclip.copy(text_m)
+    elif copy_mode in (
+        "chat",
+        # "chat2",
+        "rawchat",
+    ):
+        clipboard_copy(text_m)
 
     elif copy_mode == "text":
-        pyperclip.copy(text)
+        clipboard_copy(text)
 
     return simple_obj(
         text=text,
         text_chat=text_m,
     )
-
-
-def chatml_response_process(response, copy_mode="default"):
-    for choice in response["choices"]:
-        text = choice["message"]["content"]
-
-        chatml_response_text_process(text)
-        print(text)
-        print("-------")
 
 
 def writegpt_process(messages_lst):
@@ -114,7 +195,7 @@ def writegpt_process(messages_lst):
         capture_output=True,
     ).stdout
 
-    pyperclip.copy(out)
+    clipboard_copy(out)
     return out
 
 
@@ -122,26 +203,41 @@ def openai_chat_complete(
     *args,
     model="gpt-3.5-turbo",
     messages=None,
+    stream=True,
     interactive=False,
     copy_last_message=None,
-    bell=None,
+    trim_p=True,
     **kwargs,
 ):
+    if model in (
+        "gpt-4-turbo",
+        "4t",
+    ):
+        model = "gpt-4-1106-preview"
+
     if interactive:
         if copy_last_message is None:
             copy_last_message = True
-        if bell is None:
-            bell = True
+
+    if messages is not None:
+        if trim_p:
+            #: Trim the messages:
+            for message in messages:
+                message["content"] = message["content"].strip()
 
     try:
         while True:
             if copy_last_message:
                 last_message = messages[-1]["content"]
-                pyperclip.copy(last_message)
+                clipboard_copy(last_message)
 
             try:
                 return openai.ChatCompletion.create(
-                    *args, model=model, messages=messages, **kwargs
+                    *args,
+                    model=model,
+                    messages=messages,
+                    stream=stream,
+                    **kwargs,
                 )
             except openai.error.RateLimitError:
                 print(
@@ -151,12 +247,14 @@ def openai_chat_complete(
                 )
                 time.sleep(10)  #: in seconds
     finally:
-        if bell:
-            bell_gpt()
+        pass
 
 
 ###
 def truncate_by_tokens(text, length=3500, model="gpt-3.5-turbo"):
+    #: @deprecated?
+    #: @alt =ttok=
+    ##
     encoder = tiktoken.encoding_for_model(model)
 
     encoded = encoder.encode(text)
