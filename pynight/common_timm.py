a60e8095213b975fa02b6a018ad70ee4d7aa9caa
NightMachinery <feraidoonmehri@gmail.com>
2024-03-28 18:34:52 +0330

    fixed previous commit

--- pynight/common_timm.py
+++ pynight/common_timm.py
@@ -90,9 +90,9 @@ def patch_info_from_name(
         ##
 
         model_size_pattern = None
-        if any(model_name.startswith(prefix) for prefix in [
-                "vit_",
-                "eva(?:02)?_",
+        if any(re.search(pat, model_name) for pat in [
+                "^vit_",
+                "^eva(?:02)?_",
         ]):
             #: `eva02_tiny_patch14_336.mim_in22k_ft_in1k`
             ##
