aa4dd8793da6c12e957dfdd29b1ccb13fa8d3e74
NightMachinery <feraidoonmehri@gmail.com>
2024-07-09 00:46:01 +0330

    added hostname_get, mb2_p

--- pynight/common_hosts.py
+++ pynight/common_hosts.py
@@ -1,9 +1,23 @@
 import os
 
 
+def hostname_get():
+    return os.uname()[1]
+
+
+##
 def mmd1_p():
     #: checks if `/home/mmd` exists:
     if os.path.exists("/home/mmd"):
         return True
     else:
         return False
+
+
+##
+def mb2_p():
+    #: hostname == 'mb2.local'
+    return hostname_get() == "mb2.local"
+
+
+##
