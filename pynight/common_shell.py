42d676f16a185548b49075f1da3b3a2c086df168
NightMachinery <feraidoonmehri@gmail.com>
2024-08-12 17:35:51 +0330

    added getenv2

--- pynight/common_shell.py
+++ pynight/common_shell.py
@@ -1,9 +1,24 @@
+from os import getenv
+
+
 ##
 def str_falsey_to_none(string):
     if not string:
         return None
     else:
         return string
+
+
+##
+def getenv2(injected_value, env_var, default):
+    """
+    Retrieves the value from either an injected value (if not None), an environment variable, or the default. Also runs str_falsey_to_none on the default value or the one from the environment.
+    """
+
+    if injected_value is None:
+        return str_falsey_to_none(getenv(env_var, default=default))
+
+    return injected_value
 
 
 ##
