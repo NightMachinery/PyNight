6f4d71ae3fd34c5ccb0ed4ab7d1d02cec58f0ec1
Feraidoon Mehri <feraidoonmehri@gmail.com>
2024-09-30 18:25:31 +0330

    added model_needs_MLP_DU_p

--- pynight/common_model_name.py
+++ pynight/common_model_name.py
@@ -1,11 +1,22 @@
 open_clip_sep = ".OC."
 # open_clip_sep = "+OC+"
 
+
 ##
 def model_name_eva2_p(model_name):
-    return model_name is not None and 'eva02' in model_name.lower()
+    return model_name is not None and "eva02" in model_name.lower()
+
 
 def model_name_clip_p(model_name):
     return model_name is not None and open_clip_sep in model_name
+
+
+def model_needs_MLP_DU_p(model_name: str) -> bool:
+    specific_models = {
+        "gmixer_24_224.ra3_in1k",
+        "vit_giant_patch14_dinov2",
+    }
+    return model_name_eva2_p(model_name) or model_name in specific_models
+
 
 ##
