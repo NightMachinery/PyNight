0fa6ddeffad5dad146aada322011219e3929fc6b
NightMachinery <feraidoonmehri@gmail.com>
2024-10-14 15:18:54 +0330

    ipdb_enable: added bell, jupyter mode

--- pynight/common_debugging2.py
+++ pynight/common_debugging2.py
@@ -11,6 +11,9 @@ from pynight.common_telegram import (
     tlg_chat_id_default,
 )
 from pynight.common_telegram import send as tlg_send
+from pynight.common_bells import (
+    bell_call_remote,
+)
 from brish import z
 from IPython.core import ultratb
 
@@ -24,13 +27,21 @@ except ImportError:
 
 ##
 def ipdb_enable(
-    disable_in_jupyter_p=True,
     tlg_chat_id=tlg_chat_id_default,  #: Use None to disable
     torch_oom_mode="no_pdb",
     non_interactive_exceptions="auto",
     # non_interactive_exceptions=None,
     non_interactive_base_exception_p=True,
+    jupyter_mode=True,
+    bell_name="bell-python-error",
+    jupyter_bell_name="bell-jupyter-error",
 ):
+    in_jupyter_p = jupyter_p()
+    #: jupyter_p returns False for IPython terminal shells
+
+    if in_jupyter_p and jupyter_mode == "disabled":
+        return
+
     if non_interactive_exceptions is None:
         non_interactive_exceptions = []
 
@@ -46,9 +57,6 @@ def ipdb_enable(
             ConnectionError,
             OSError,
         ]
-
-    if disable_in_jupyter_p and jupyter_p():
-        return
 
     pdb_excepthook = ultratb.FormattedTB(
         mode="Context",
@@ -83,21 +91,43 @@ def ipdb_enable(
 
             threading.Thread(target=send_telegram_notification, daemon=True).start()
 
-        if TORCH_AVAILABLE and isinstance(exc_value, torch.cuda.OutOfMemoryError):
-            print("CUDA OutOfMemoryError occurred.", file=sys.stderr)
-            if torch_oom_mode == "no_pdb":
-                print("Not entering debugger.", file=sys.stderr)
+        if in_jupyter_p:
+            bell_call_remote(jupyter_bell_name)
+
+            return non_pdb_excepthook(exc_type, exc_value, exc_traceback)
+        else:
+            bell_call_remote(bell_name)
+
+            if TORCH_AVAILABLE and isinstance(exc_value, torch.cuda.OutOfMemoryError):
+                print("CUDA OutOfMemoryError occurred.", file=sys.stderr)
+                if torch_oom_mode == "no_pdb":
+                    print("Not entering debugger.", file=sys.stderr)
+                    return non_pdb_excepthook(exc_type, exc_value, exc_traceback)
+
+            elif (
+                non_interactive_base_exception_p and not issubclass(exc_type, Exception)
+            ) or (
+                any(isinstance(exc_value, exc) for exc in non_interactive_exceptions)
+            ):
                 return non_pdb_excepthook(exc_type, exc_value, exc_traceback)
 
-        if non_interactive_base_exception_p and not issubclass(exc_type, Exception):
-            return non_pdb_excepthook(exc_type, exc_value, exc_traceback)
-
-        if any(isinstance(exc_value, exc) for exc in non_interactive_exceptions):
-            return non_pdb_excepthook(exc_type, exc_value, exc_traceback)
-
-        return pdb_excepthook(exc_type, exc_value, exc_traceback)
+            else:
+                return pdb_excepthook(exc_type, exc_value, exc_traceback)
 
     sys.excepthook = excepthook_wrapper
+    # print(
+    #     f"hooked sys.excepthook: {sys.excepthook}",
+    #     file=sys.stderr,
+    # )
+
+    if in_jupyter_p:
+        from IPython.core.interactiveshell import InteractiveShell
+
+        def custom_exc(shell, etype, evalue, tb, tb_offset=None):
+            excepthook_wrapper(etype, evalue, tb)
+
+        shell = InteractiveShell.instance()
+        shell.set_custom_exc((BaseException,), custom_exc)
 
 
 ##
