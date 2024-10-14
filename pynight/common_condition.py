99799d2b06e7615e35e1c42bde12d02b8c6bdaa7
NightMachinery <feraidoonmehri@gmail.com>
2024-10-14 15:19:26 +0330

    blackened

--- pynight/common_condition.py
+++ pynight/common_condition.py
@@ -18,10 +18,13 @@ def jupyter_p_v2():
         shell = get_ipython().__class__.__name__
         if shell == "ZMQInteractiveShell":
             return True  # Jupyter notebook or qtconsole
+
         elif shell == "TerminalInteractiveShell":
             return False  # Terminal running IPython
+
         else:
             return False  # Other type (?)
+
     except NameError:
         return False  # Probably standard Python interpreter
 
