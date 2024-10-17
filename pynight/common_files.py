3c3ec40643b322c863e1a1169160d3c2d38b8839
NightMachinery <feraidoonmehri@gmail.com>
2024-10-17 18:56:43 +0330

    hdd_free_get: added path

--- pynight/common_files.py
+++ pynight/common_files.py
@@ -201,19 +201,25 @@ class open_file:
 
 
 ##
-def hdd_free_get(unit="GB"):
-    total, used, free = shutil.disk_usage("/")
+def hdd_free_get(
+    *,
+    unit="GB",
+    path="/",
+):
+    total, used, free = shutil.disk_usage(path)
 
     if unit == "GB":
-        free_space = free / (1024 ** 3)  # Convert to GB
+        free_space = free / (1024**3)  # Convert to GB
     elif unit == "MB":
-        free_space = free / (1024 ** 2)  # Convert to MB
+        free_space = free / (1024**2)  # Convert to MB
     elif unit == "TB":
-        free_space = free / (1024 ** 4)  # Convert to TB
+        free_space = free / (1024**4)  # Convert to TB
     elif unit == "B":
         free_space = free
     else:
         raise ValueError(f"Invalid unit: '{unit}'")
 
     return free_space
+
+
 ##
