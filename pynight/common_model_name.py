f92ac69807de567b13de995e4fe451c26fa0da3e
NightMachinery <feraidoonmehri@gmail.com>
2024-10-21 21:56:46 +0330

    added model_name_mixer_p

--- pynight/common_model_name.py
+++ pynight/common_model_name.py
@@ -10,6 +10,9 @@ def model_name_eva2_p(model_name):
 def model_name_clip_p(model_name):
     return model_name is not None and open_clip_sep in model_name
 
+def model_name_mixer_p(model_name):
+    return model_name is not None and "mixer" in model_name
+
 
 def model_needs_MLP_DU_p(model_name: str) -> bool:
     #: @todo Add MambaOut
