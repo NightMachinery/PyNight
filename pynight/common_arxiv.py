e99d0b831251484292ac9b9788386f450c2613d3
NightMachinery <feraidoonmehri@gmail.com>
2024-10-30 14:02:08 +0330

    new common_arxiv.py

---
+++ pynight/common_arxiv.py
@@ -0,0 +1,21 @@
+import re
+
+
+def normalize_path_for_arxiv(
+    path,
+    *,
+    disallowed_regex=r"[ :&\\]",
+):
+    """
+    Normalize a file path by replacing spaces, slashes, colons, ampersands,
+    and backslashes with underscores.
+
+    Args:
+        path (str): The file path to normalize
+
+    Returns:
+        str: The normalized file path
+    """
+    path = re.sub(disallowed_regex, "_", path)
+
+    return path
